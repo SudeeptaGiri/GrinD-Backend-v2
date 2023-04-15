e55f2c5318a0866bac1d093c5db468b8b3fef539
Aparesh <kraparesh@gmail.com>
2023-04-16 01:09:44 +0530

    Add: Back-end

---
+++ index.js
@@ -0,0 +1,30 @@
+// TODO: Fix subscription server
+
+require('dotenv').config();
+const {ApolloServer} = require('apollo-server');
+const {PubSub} = require('graphql-subscriptions');
+const mongoose = require('mongoose');
+
+
+const resolvers = require('./graphql/resolvers');
+const typeDefs = require('./graphql/typeDefs');
+
+const pubsub = new PubSub();
+
+// We need type definitions for GQL, hence we are declaring it below
+
+const server = new ApolloServer({
+    typeDefs, 
+    resolvers, 
+    context: ({ req }) => ({ req, pubsub })
+});
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log(`Successfully connected to the database`)
+        return server.listen({port: 5000});
+    })
+    .then((res) => {
+        console.log(`Server running at ${res.url}`);
+    });
+    
